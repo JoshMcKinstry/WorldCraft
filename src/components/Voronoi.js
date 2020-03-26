05bed2f36b579cf1569ed72902719a39505263c6
Alfredo Marroquin <amarroq2@msudenver.edu>
2020-03-26 17:00:22 -0600

    Component organization

--- src/components/Voronoi.js
+++ src/components/Voronoi.js
@@ -2,15 +2,14 @@
 import * as d3 from 'd3';
 import {Delaunay} from 'd3-delaunay';
 
-//VORONOI
-
+//DELAUNAY VORONOI
 const draw = (props) =>{	 
 	const w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 	const h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
-	const svg = d3.select('.viz').append('svg')
+	d3.select('.viz').append('svg')
 	.attr('height',h)
-	.attr('weight',w)
+	.attr('width',w)
 	.attr('id','svg-viz');
 	
 	const nPoints = props.points;
@@ -22,23 +21,16 @@ const draw = (props) =>{
 	const delaunay = Delaunay.from(points);
 	const voronoi = delaunay.voronoi([0,0,w,h]);
 
-	const mesh = d3.select('#svg-viz')
+	d3.select('#svg-viz').selectAll("voronoi")
+		.data(props.points)
+		.append('svg:circle')
 		.attr("fill", "none")
 		.attr("stroke", "#ccc")
 		.attr("stroke-width", 1)
-		.enter()
-		.attr("d", voronoi.render());
-
-	var count = 0;
-	while (points.length>count){
-		svg.append("voronoi")
-		.attr("cx", points[count][0])
-		.attr("cy", points[count][1])
-		.attr("r", 1);
-		count++;
-	}
-	mesh.attr("d",voronoi.render());
-	
+		.attr("d", voronoi.render())
+		.enter();
+		
+		
 
 }
 
