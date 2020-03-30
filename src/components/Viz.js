4ef71185fceb6b1a779188c238ae0e05d7bd76f8
Alfredo Marroquin <amarroq2@msudenver.edu>
2020-03-30 10:54:49 -0600

    minor changes

--- src/components/Viz.js
+++ src/components/Viz.js
@@ -2,13 +2,15 @@ import React, { useEffect } from 'react';
 import draw from './Voronoi.js';
 import * as d3 from 'd3';
 
+
 //fence like component to create a border between React and D3
 //using React Hooks
 const Viz = (props) => {
   useEffect(() => {
     d3.select('.voronoi > *').remove();
-    draw(props)
+    draw(props);
   }, [props.voronoi.length]); 
+
   return <div className = 'voronoi'/>
 }
 
--- src/components/Voronoi.js
+++ src/components/Voronoi.js
@@ -5,23 +5,45 @@ const draw = (props) =>{
     const w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 	const h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
-	const width = parseInt(w,10)
-	const height = parseInt(h,10)
-	console.log(width)
-	console.log(height)
+	const width = parseInt(w,10);
+	const height = parseInt(h,10);
 	
 
-    d3.select('.voronoi').append('svg')
+    const svg = d3.select('.voronoi').append('svg')
       .attr('height', h)
       .attr('width', w)
-      .attr('id', 'svg-viz');
+	  .attr('id', 'svg-viz');
 
 	const n = props.voronoi;
 	const num = parseInt(n,10);
 	const particles = Array.from({length: num}, () => [Math.random() * width, Math.random() * height]);
     const delaunay = Delaunay.from(particles);
-	const voronoi = delaunay.voronoi([0.5,0.5,w-0.5,h-0.5]);
+	const voronoi = delaunay.voronoi(particles);
 	
-}
+	//container that has voronoi cells to draw
+	const cells = svg.append("svg:g")
+		.attr("id", "cells")
+    	.attr('fill','none')
+    	.attr('stroke','#666')
+		.attr('stroke-opacity',0);
+		
+	//assigns each point to g
+	var g = cells.selectAll("g")
+            .data(particles)
+            .enter()
+			.append("svg:g");
+
+	//draw voronoi
+	g.append("svg:path")
+        .attr("class", "cell")
+        .attr("d", function(d, i) {
+            //m - pen down
+            //voronoi[i],join('L') - draw lines joining each point in voronoi[i]
+            //z - pen up
+            return "M" + voronoi[i].join("L") + "Z";
+        });
+			
+
+};
 
 export default draw;
